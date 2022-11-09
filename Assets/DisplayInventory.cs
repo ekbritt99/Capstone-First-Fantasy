64fe30f234639e5cea7712993804762f8879031f
cdrez <55902670+cdrez@users.noreply.github.com>
2022-11-08 21:31:49 -0500

    Fixed UI resolution scaling.

--- Assets/DisplayInventory.cs
+++ Assets/DisplayInventory.cs
@@ -9,7 +9,8 @@ public class DisplayInventory : MonoBehaviour
     public GameObject inventoryPrefab;
     public InventoryObject inventory;
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
-    
+    public GameObject grid;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +20,21 @@ public class DisplayInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateDisplay();
     }
 
     public void CreateDisplay()
     {
-        GameObject grid = GameObject.Find("Grid");
+        grid = GameObject.Find("InventoryGrid");
         for (int i = 0; i < inventory.container.Count; i++)
         {
             var obj = Instantiate(inventoryPrefab);
-            obj.transform.GetChild(0).GetComponent<Image>().sprite = inventory.container[i].item.uiDisplay;
-            obj.transform.parent = grid.transform;
+            obj.transform.SetParent(grid.transform);
         }
+    }
+
+    public void UpdateDisplay()
+    {
+        //obj.transform.GetChild(0).GetComponent<Image>().sprite = inventory.container[i].item.uiDisplay;
     }
 }
