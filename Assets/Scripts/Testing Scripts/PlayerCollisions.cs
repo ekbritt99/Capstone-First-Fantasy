954256534f05e14f488193e68b702f12ea2d2a0b
Ethan Brittain <70303464+ekbritt99@users.noreply.github.com>
2022-11-13 17:55:35 -0500

    Added simple Game Manager to switch between scenes
    Detects collision between player and set trigger object and then switches to corresponding scene.

---
+++ Assets/Scripts/Testing Scripts/PlayerCollisions.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    public GameObject gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Scene Trigger")
+        {
+            gameManager.SendMessage("GoToOverWorld");
+        }
+        if (collision.gameObject.tag == "Scene Trigger 2")
+        {
+            gameManager.SendMessage("GoToVillage");
+        }
+
+    }
+}
