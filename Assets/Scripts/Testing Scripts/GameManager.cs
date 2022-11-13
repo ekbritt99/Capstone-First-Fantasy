0e52aacbe60054677a19bb3b2f85debbdcc223f6
Ethan Brittain <70303464+ekbritt99@users.noreply.github.com>
2022-11-13 18:12:24 -0500

    Added pause functionality to game manager

--- Assets/Scripts/Testing Scripts/GameManager.cs
+++ Assets/Scripts/Testing Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +9,32 @@ public class GameManager : MonoBehaviour
     public string villageScene = "Test Village Scene";
     public string overworldScene = "Test World Scene";
 
-    
+    bool pause = false;
+    public GameObject pauseMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Pause"))
+        {
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(!pause);
+        if (!pause)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+        pause = !pause;
     }
 
     void GoToVillage()
