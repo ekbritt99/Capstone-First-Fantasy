094a771842e56d32679e099bb35c01d9704b349e
Ethan Brittain <70303464+ekbritt99@users.noreply.github.com>
2023-04-02 23:45:05 -0400

    Fixed player hp HUD

--- Assets/Scripts/HUD Scripts/DisplayPlayerHP.cs
+++ Assets/Scripts/HUD Scripts/DisplayPlayerHP.cs
@@ -66,6 +66,37 @@ public class DisplayPlayerHP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currHealth = playerStats.currentHP;
+        maxHealth = playerStats.maxHP;
+
+        float cHealth = (float)currHealth;
+        float mHealth = (float)maxHealth;
+        // hpBarSlider. = new Vector2((cHealth / mHealth) * 14.4f, 8f);
+        Debug.Log(hpBarSlider);
+        hpBarSlider.value = cHealth / mHealth;
+
+        weaponSprite = Weapon.GetComponent<Image>();
+        int currentWeapon = playerEquipment.container.Items[4].item.ID;
+        if (currentWeapon == 1)
+        {
+            weaponSprite.sprite = katanaSprite;
+            weaponSprite.color = Color.white;
+            // Weapon.transform.localScale = new Vector3(0.09564409f, 0.09564409f, 0.09564409f);
+        }
+        if (currentWeapon == 3)
+        {
+            weaponSprite.sprite = maceSprite;
+            weaponSprite.color = Color.white;
+        }
+        if (currentWeapon == 4)
+        {
+            weaponSprite.sprite = romanSprite;
+            weaponSprite.color = Color.white;
+        }
+        if (currentWeapon == 4)
+        {
+            weaponSprite.sprite = scimitarSprite;
+            weaponSprite.color = Color.white;
+        }
     }
 }
